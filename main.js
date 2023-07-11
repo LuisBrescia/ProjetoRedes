47b0d333feae73172b794da57fb1b1d91e678ac9
Luis Brescia <luisfelipebrescia@gmail.com>
2023-07-11 10:50:09 -0300

    Resolvendo problema do deploy

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 import 'bootstrap-icons/font/bootstrap-icons.css'
-import { setupCounter } from './counter.js'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'jquery/dist/jquery.min.js'
-import '@popperjs/core/dist/umd/popper.js'
+// import '@popperjs/core/dist/umd/popper.js'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import './style.css'
 
