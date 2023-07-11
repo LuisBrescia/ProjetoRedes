f6f9e0e8cad06d695526749c55ad6ac9f216c174
Luis Brescia <luisfelipebrescia@gmail.com>
2023-07-11 10:46:11 -0300

    Navbar agora possibilita trocar entre unidades de conteúdo

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-import './style.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import { setupCounter } from './counter.js'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import 'jquery/dist/jquery.min.js'
+import '@popperjs/core/dist/umd/popper.js'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import './style.css'
 
