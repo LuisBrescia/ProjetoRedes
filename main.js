ffb6e678c5fb8a21a1c2e5dbf203dd6cb094cc9c
Luis Brescia <luisfelipebrescia@gmail.com>
2023-07-12 12:50:58 -0300

    Cards agora são interativos

--- main.js
+++ main.js
@@ -15,49 +15,81 @@ $(document).ready(function () {
     } else {
         $('#mudaTema').toggleClass('bi-sun-fill bi-circle-half');
     }
-    
+
 
     // * Função que define a unidade a ser estudada
     $('[name="unidade"]').change(function () {
         unidade = $(this).val()
         console.log(unidade)
         localStorage.setItem('unidade', unidade);
-        carregaConteudo(unidade);
+        carregaConteudo(unidade).then(function () {
+
+            $('#mudaTema').click(function () {
+                $(this).toggleClass('bi-sun-fill bi-moon-stars-fill');
+                tema = (tema == 'dark') ? 'light' : 'dark';
+                alteraTema(tema);
+                localStorage.setItem('tema', tema);
+                console.log(tema)
+            })
+
+            $('.card').click(function () {
+                console.log("algo");
+                console.log("titulo", $(this).find('.card-title').text());
+                $('.modal-title').text($(this).find('.card-title').text() + ' Unidade ' + unidade);
+                $('.modal-body h1').text('Deseja iniciar o ' + $(this).find('.card-title').text() + '?');
+                $('.modal-body label:nth-child(1)').text('Tempo estimado: ' + Math.floor(Math.random() * 60) + ' minutos');
+                $('.modal-body label:nth-child(2)').text($(this).find('.card-text').text());
+                $('.modal-footer button').text('Iniciar ' + $(this).find('.card-title').text());
+            });
+        });
     })
 
-    carregaConteudo(unidade);
-    const toastLiveExample = document.getElementById('liveToast')
-    const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
-    $('#enviaQuestionario').click(function () {
-        $('.toast-body').text('Questionário indisponível no momento...');
-        toastBootstrap.show()
-    })
+    carregaConteudo(unidade).then(function () {
 
-    $('#mudaTema').click(function () {
-        $(this).toggleClass('bi-sun-fill bi-moon-stars-fill');
-        tema = (tema == 'dark') ? 'light' : 'dark';
-        alteraTema(tema);
-        localStorage.setItem('tema', tema);
-        console.log(tema)
-    })
+        const toastLiveExample = document.getElementById('liveToast')
+        const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+        $('#enviaQuestionario').click(function () {
+            $('.toast-body').text('Indisponível no momento...');
+            toastBootstrap.show()
+        })
+
+        $('#mudaTema').click(function () {
+            $(this).toggleClass('bi-sun-fill bi-moon-stars-fill');
+            tema = (tema == 'dark') ? 'light' : 'dark';
+            alteraTema(tema);
+            localStorage.setItem('tema', tema);
+            console.log(tema)
+        })
+
+        $('.card').click(function () {
+            console.log("algo");
+            console.log("titulo", $(this).find('.card-title').text());
+            $('.modal-title').text($(this).find('.card-title').text() + ' Unidade ' + unidade);
+            $('.modal-body h1').text('Deseja iniciar o ' + $(this).find('.card-title').text() + '?');
+            $('.modal-body label:nth-child(1)').text('Tempo estimado: ' + Math.floor(Math.random() * 60) + ' minutos');
+            $('.modal-body label:nth-child(2)').text($(this).find('.card-text').text());
+            $('.modal-footer button').text('Iniciar ' + $(this).find('.card-title').text());
+        });
+    });
 })
 
 function alteraTema(tema) {
-    $('body').toggleClass('bg-dark bg-white text-white text-dark');
-    $('nav').attr('data-bs-theme', tema);
+    $('nav, aside, .modal-content, #liveToast, .toast-header').attr('data-bs-theme', tema);
+    $('nav button, .modal-footer button').toggleClass('btn-1 btn-2');
     $('nav').toggleClass('bg-white bg-dark');
-    $('aside').attr('data-bs-theme', tema);
-    $('.card').toggleClass('bg-dark bg-white text-white text-dark');
-    $('nav button').toggleClass('btn-1 btn-2');
-    $('.modal-footer button').toggleClass('btn-1 btn-2');
-    $('.modal-header button').toggleClass('btn-close-white');
-    $('.modal-content').toggleClass('bg-dark bg-white text-white text-dark');
-    $('.modal-content').attr('data-bs-theme', tema);
-    $('#liveToast').toggleClass('bg-dark bg-white text-white text-dark');
+    $('.toast-header').toggleClass('text-black bg-light bg-escuro text-white');
+    $('body, .card, .modal-content, #liveToast').toggleClass('bg-dark bg-white text-white text-dark');
+    $('.modal-header button, .toast-header button').toggleClass('btn-close-white');
 }
 
 function carregaConteudo(unidade) {
-    $('#conteudo').load('/unidade' + unidade + '.html #unidade' + unidade + '_conteudo');
-    $('#topico').load('/unidade' + unidade + '.html #unidade' + unidade + '_topicos');
-    // $('#interativo').load('/unidade'+ unidade +'.html #unidade'+ unidade +'_interativo');
+    return new Promise(function (resolve, reject) {
+        $('#topico').load('/unidade' + unidade + '.html #unidade' + unidade + '_topicos', function () {
+            $('#interativo').load('/unidade' + unidade + '.html #unidade' + unidade + '_interativo', function () {
+                $('#conteudo').load('/unidade' + unidade + '.html #unidade' + unidade + '_conteudo', function () {
+                    resolve(); // Resolve a promessa após carregar todos os elementos
+                });
+            });
+        });
+    });
 }
